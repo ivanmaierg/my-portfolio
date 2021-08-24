136384cfe2931e8e3b012a221d8b34a7e87bf8d7
ivanmaierg <ivan_maier@hotmail.com>
2021-08-23 23:45:35 -0300

    fix: Fix GlassContainer sm padding

--- components/Projects/Projects.js
+++ components/Projects/Projects.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Projects = () => {
     return (
-        <div>
+        <div style={{display:'flex',justifyContent:'center',alignItems:'center',width:'100%',margin:'auto',height:'5rem'}}>
             <h3>Projects</h3>
         </div>
     )
