0ffd20b31932e32e66e0ac1ec63436c3504b5dcd
ivanmaierg <ivan_maier@hotmail.com>
2021-08-25 00:29:35 -0300

    fix: Eliminate Next  Link from call to action section

--- components/CallToAction/CallToAction.js
+++ components/CallToAction/CallToAction.js
@@ -1,8 +1,6 @@
 /* eslint-disable react/no-unescaped-entities */
 import React from 'react'
 import { ButtonAction, Container, InfoContainer } from './CallToAction.styles'
-
-import Link from 'next/link';
 import { Portrait } from '../About/About.styles';
 
 const CallToAction = () => {
@@ -13,7 +11,7 @@ const CallToAction = () => {
                     <h1>Ivan Maier Gallardo</h1>
                     <h3>Desarrollador Front end</h3>
                 <p>Soy un estudiante de  <strong> informática </strong> y <strong>desarrollador web </strong> interesado en  la construccón de <strong>interfaces de usuario</strong> . Mis intereses giran en torno a el ecositema de tecnologías de javascript y la plataforma web en general.</p>
-                <Link href="#About"><ButtonAction href="#About" whileHover={{ scale: 1.2, originX: 0.3,originY:0, filter:'brightness(1.3)', }} transition={{type:'spring',stiffness:'120'}}><span>About me</span></ButtonAction></Link>
+                <ButtonAction href="#About" whileHover={{ scale: 1.2, originX: 0.3,originY:0, filter:'brightness(1.3)', }} transition={{type:'spring',stiffness:'120'}}><span>About me</span></ButtonAction>
                 </InfoContainer>
             <Portrait initial={{ scale: 0 }} animate={{ scale: 1 }} transition={{ delay: 0.5, type: 'spring', stiffness: 120 }}>
                 <img alt='my portrait' style={{ height: '100%', width: '100%', objectFit: 'cover' }} src='/portrait.jpg'></img>
