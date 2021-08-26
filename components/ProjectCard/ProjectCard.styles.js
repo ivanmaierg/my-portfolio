b44c9cb249988e1ee9a9974a6f3dcc34013b231b
ivanmaierg <ivan_maier@hotmail.com>
2021-08-26 18:39:03 -0300

    feat: Add projects section and projectCard component

---
+++ components/ProjectCard/ProjectCard.styles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+import { GlassContainer } from "../../styles/GlobalComponents/GlassContainer.styles";
+
+export const ProjectCardContainer = styled(GlassContainer)` 
+    display:flex;
+    flex-direction:column;
+    max-width:60rem;
+`
