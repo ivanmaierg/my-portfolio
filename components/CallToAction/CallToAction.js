fc33052d38380bfb538a2ad8eec8c9024e2ed5f1
ivanmaierg <ivan_maier@hotmail.com>
2021-08-25 21:24:16 -0300

    fix: Fix typo on CalltoAction container

--- components/CallToAction/CallToAction.js
+++ components/CallToAction/CallToAction.js
@@ -10,7 +10,7 @@ const CallToAction = () => {
                     <h2 >Hola, mi nombre es </h2>
                     <h1>Ivan Maier Gallardo</h1>
                     <h3>Desarrollador Front end</h3>
-                <p>Soy un estudiante de  <strong> informática </strong> y <strong>desarrollador web </strong> interesado en  la construccón de <strong>interfaces de usuario</strong> . Mis intereses giran en torno a el ecositema de tecnologías de javascript y la plataforma web en general.</p>
+                <p>Soy un estudiante de  <strong> informática </strong> y <strong>desarrollador web </strong> interesado en  la construccón de <strong>interfaces de usuario</strong> . Mis intereses giran en torno a el ecosistema de tecnologías de javascript y la plataforma web en general.</p>
                 <ButtonAction href="#About" whileHover={{ scale: 1.2, originX: 0.3,originY:0, filter:'brightness(1.3)', }} transition={{type:'spring',stiffness:'120'}}><span>About me</span></ButtonAction>
                 </InfoContainer>
             <Portrait initial={{ scale: 0 }} animate={{ scale: 1 }} transition={{ delay: 0.5, type: 'spring', stiffness: 120 }}>
