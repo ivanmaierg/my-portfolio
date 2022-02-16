1503d175f827a36c15573b4af29915edc8f3fcf1
ivanmaierg <ivan_maier@hotmail.com>
2022-02-16 18:34:33 -0300

    feat: add another project

--- components/ProjectCard/ProjectCard.styles.js
+++ components/ProjectCard/ProjectCard.styles.js
@@ -3,18 +3,18 @@ import styled from "styled-components";
 import { GlassContainer } from "../../styles/GlobalComponents/GlassContainer.styles";
 import { Item } from "../About/About.styles";
 
-export const ProjectCardContainer = styled(GlassContainer)` 
-    display:flex;
-    flex-direction:column;
-    height:100%;
-    & h4 {
-        height:auto;
-        font-size:2.5rem;
-        margin:2rem 0;
-    };
-    @media ${(props) => props.theme.breakpoints.sm}{
-        padding:2rem;
-    }
+export const ProjectCardContainer = styled(GlassContainer)`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  & h4 {
+    height: auto;
+    font-size: 2.5rem;
+    margin: 2rem 0;
+  }
+  @media ${(props) => props.theme.breakpoints.sm} {
+    padding: 2rem;
+  }
 `;
 
 export const ProjectCardContent = styled.div`
@@ -25,62 +25,59 @@ export const ProjectCardContent = styled.div`
     align-items: stretch;
     justify-content: center;
 }
-`
+`;
 
 export const ProjectCardDescription = styled.div`
-    height:50%;
-    display:flex;
-    flex-direction:column;
-    justify-content:space-between;
-    color:white;
-    min-height:10rem;
-    & p {
-        padding:2rem 0;
-        text-overflow:ellipsis;
-        overflow:hidden;
-        color:${(props) => props.theme.colors.paragraph};
-    }
-    > p:first-child {
-        font-size:2rem;
-        font-weight:bold;
-        color:${(props) => props.theme.colors.white};
-    }
-    & ul {
-        padding:1rem 0;
-        display: flex;
-        flex-direction:row;
-        flex-wrap:wrap;
-
-    }
-
-`
-export const TechnologiesSpan  = styled(Item)`
+  height: 50%;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  color: white;
+  min-height: 10rem;
+  padding: 2rem 0;
+  & p {
+    padding: 1rem;
+    text-overflow: ellipsis;
+    overflow: hidden;
+    color: ${(props) => props.theme.colors.paragraph};
+  }
+  > p:first-child {
+    font-size: 2rem;
+    font-weight: bold;
+    color: ${(props) => props.theme.colors.white};
+  }
+  & ul {
+    padding: 1rem 0;
     display: flex;
-    justify-content:center;
-    align-items:center;
-    padding:0rem 1rem;
-    min-width:5rem;
-    margin:0.5rem;
-    border-radius:10px;
-    height:3rem;
-    p {
-        color:white;
-        font-size:1.5rem;
-        font-weight:bold;
-    }
-`
-
+    flex-direction: row;
+    flex-wrap: wrap;
+  }
+`;
+export const TechnologiesSpan = styled(Item)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0rem 1rem;
+  min-width: 5rem;
+  margin: 0.5rem;
+  border-radius: 10px;
+  height: 3rem;
+  p {
+    color: white;
+    font-size: 1.5rem;
+    font-weight: bold;
+  }
+`;
 
 export const ProjectImagePreview = styled(Image)`
-    border-radius:10px;
-    height:50%;
-    object-fit:cover;
+  border-radius: 10px;
+  height: 50%;
+  object-fit: cover;
 `;
 export const ProjectLink = styled.div`
-    display:flex;
-    justify-content:end;
-    > a:first-child{
-        margin-right:1rem;
-    }
-
-`
+  display: flex;
+  justify-content: end;
+  > a:first-child {
+    margin-right: 1rem;
+  }
+`;
--- data/data.js
+++ data/data.js
@@ -1,139 +1,156 @@
 export const educationInfo = [
-    {
-        id: 1,
-        title: 'Analista Programador Universitario',
-        place: 'Facultad de Informática - UNLP',
-        date: '2020 - En curso',
-    },
-    {
-        id: 2,
-        title: 'Escuela de Javascript',
-        place: 'Platzi',
-        date: '2021',
-    },
-    {
-        id: 3,
-        title: 'Advanced css and, sass, flex-box, grid, animation and more',
-        place: 'Platzi',
-        date: '2021',
-    },
+  {
+    id: 1,
+    title: "Analista Programador Universitario",
+    place: "Facultad de Informática - UNLP",
+    date: "2020 - En curso"
+  },
+  {
+    id: 2,
+    title: "Escuela de Javascript",
+    place: "Platzi",
+    date: "2021"
+  },
+  {
+    id: 3,
+    title: "Advanced css and, sass, flex-box, grid, animation and more",
+    place: "Platzi",
+    date: "2021"
+  },
 
-    {
-        id: 4,
-        title: 'React de cero a experto',
-        place: 'Udemy',
-        date: '2020',
-    },
-    {
-        id: 5,
-        title: 'The Complete 2020 Web Development Bootcamp',
-        place: 'Udemy',
-        date: '2020',
-    },
-    {
-        id: 6,
-        title: 'The Complete 2020 Web Development Bootcamp',
-        place: 'Udemy',
-        date: '2020',
-    },
+  {
+    id: 4,
+    title: "React de cero a experto",
+    place: "Udemy",
+    date: "2020"
+  },
+  {
+    id: 5,
+    title: "The Complete 2020 Web Development Bootcamp",
+    place: "Udemy",
+    date: "2020"
+  },
+  {
+    id: 6,
+    title: "The Complete 2020 Web Development Bootcamp",
+    place: "Udemy",
+    date: "2020"
+  }
 ];
 
 export const Skills = [
-    {
-        id: 1,
-        name: 'Html',
-        url: '',
-    },
-    {
-        id: 2,
-        name: 'Css',
-        url: '',
-    },
-    {
-        id: 3,
-        name: 'Javascript',
-        url: '',
-    },
-    {
-        id: 6,
-        name: 'React',
-        url: '',
-    },
+  {
+    id: 1,
+    name: "Html",
+    url: ""
+  },
+  {
+    id: 2,
+    name: "Css",
+    url: ""
+  },
+  {
+    id: 3,
+    name: "Javascript",
+    url: ""
+  },
+  {
+    id: 6,
+    name: "React",
+    url: ""
+  },
 
-    {
-        id: 7,
-        name: 'Node js',
-        url: '',
-    },
-    {
-        id: 8,
-        name: 'Redux',
-        url: '',
-    },
-    {
-        id: 9,
-        name: 'Webpack',
-        url: '',
-    },
+  {
+    id: 7,
+    name: "Node js",
+    url: ""
+  },
+  {
+    id: 8,
+    name: "Redux",
+    url: ""
+  },
+  {
+    id: 9,
+    name: "Webpack",
+    url: ""
+  },
 
-    {
-        id: 10,
-        name: 'Typescript',
-        url: '',
-    },
-    {
-        id: 11,
-        name: 'Express',
-        url: '',
-    },
-    {
-        id: 12,
-        name: 'Git',
-        url: ''
-    },
-    {
-        id: 14,
-        name: 'Next.js',
-        url: '',
-    },
-]
+  {
+    id: 10,
+    name: "Typescript",
+    url: ""
+  },
+  {
+    id: 11,
+    name: "Express",
+    url: ""
+  },
+  {
+    id: 12,
+    name: "Git",
+    url: ""
+  },
+  {
+    id: 14,
+    name: "Next.js",
+    url: ""
+  }
+];
 
 export const ProjectsInfo = [
-    {
-        title:'Adviency',
-        url:'https://adviency-krwfrekr4-getsuga743.vercel.app/',
-        img:'https://res.cloudinary.com/ds3tqgxy5/image/upload/c_fit,h_480,w_768/v1642992733/projects/adviency-krwfrekr4-getsuga743.vercel.app__tsgon8.png',
-        repository:'https://github.com/Getsuga743/Adviency',
-        technologies:['React','Typescript','Css'],
-        description:'Pequeña spa producto de un challenge de <a href="https://twitter.com/goncy">@goncy.tsx</a>',
-    },
-    {
-        title: 'Cripto Barrani',
-        url: 'https://crypto-barrani.vercel.app/',
-        img: 'https://res.cloudinary.com/ds3tqgxy5/image/upload/c_fit,h_480,w_768/v1636988356/projects/Captura_de_pantalla_de_2021-11-15_11-57-31_he1brm.png',
-        repository: 'https://github.com/Getsuga743/crypto-barrani',
-        technologies: ['Nextjs', 'Chakra-ui', 'Styled-components', 'Swr', 'React'],
-        description: "Aplicación web, hecha con Next.js , Un dashboard donde ver distintos datos sobre la economía argentina y criptomonedas",
-    }, {
-        title: 'React Journal',
-        url: 'https://journal-react-app.vercel.app/',
-        img: 'https://res.cloudinary.com/ds3tqgxy5/image/upload/c_fit,h_480,w_768/v1636988587/projects/Captura_de_pantalla_de_2021-11-15_12-01-43_ydywxq.png',
-        repository: 'https://github.com/Getsuga743/Journal-React-App',
-        technologies: ['React', 'Sass', 'Redux', 'Node', 'Firebase'],
-        description: "Pequeña spa, un diario personal web, firebase como db.",
-    }, {
-        title: 'Easy landing Bank',
-        url: 'https://getsuga743.github.io/Easybank-landing/',
-        img: 'https://res.cloudinary.com/dz209s6jk/image/upload/c_fit,h_480,w_768/q_auto:good,w_900/Challenges/yezt1f56cfp2njnakpbo.jpg',
-        repository: 'https://github.com/Getsuga743/Easybank-landing',
-        technologies: ['JS', 'Sass', 'HTML5'],
-        description: 'Reto de <a href="https://www.frontendmentor.io/challenges">https://www.frontendmentor.io/challenges</a>. Maquetado de una landing page para un banco, hecha con html, sass, y javascript. Diseño full responsive.',
-    }, {
-        title: 'BuenaBit',
-        url: 'https://getsuga743.github.io/BuenaBit/',
-        img: 'https://res.cloudinary.com/ds3tqgxy5/image/upload/c_fit,h_480,w_768/v1636988718/projects/Captura_de_pantalla_de_2021-11-15_12-04-00_hkr7dr.png',
-        repository: 'https://github.com/Getsuga743/BuenaBit',
-        technologies: ['Html','Css', 'Js'],
-        description: "Maquetado de una landing para un proyecto de criptomonedas, diseño full responsive, y mobile first.",
-    },
-]
+  {
+    title: "Envio Pack Challenge",
+    url: "https://enviopack-challenge-frontend-ivan.vercel.app/",
+    img: "https://res.cloudinary.com/ds3tqgxy5/image/upload/c_fit,h_480,w_768/v1645046582/projects/Captura_de_pantalla_de_2022-02-16_18-22-52_lj49r1.png",
+    repository: "https://github.com/Getsuga743/enviopack-challenge-frontend",
+    technologies: ["React", "styled-components", "Context Api", "react-router"],
+    description:
+      "Challenge hecho para la empresa enviopack, se trata de la creación de un pequeño cátalogo de ecommerce, con carrito."
+  },
+
+  {
+    title: "Adviency",
+    url: "https://adviency-krwfrekr4-getsuga743.vercel.app/",
+    img: "https://res.cloudinary.com/ds3tqgxy5/image/upload/c_fit,h_480,w_768/v1642992733/projects/adviency-krwfrekr4-getsuga743.vercel.app__tsgon8.png",
+    repository: "https://github.com/Getsuga743/Adviency",
+    technologies: ["React", "Typescript", "Css"],
+    description:
+      'Pequeña spa producto de un challenge de <a href="https://twitter.com/goncy">@goncy.tsx</a>'
+  },
+  {
+    title: "Cripto Barrani",
+    url: "https://crypto-barrani.vercel.app/",
+    img: "https://res.cloudinary.com/ds3tqgxy5/image/upload/c_fit,h_480,w_768/v1636988356/projects/Captura_de_pantalla_de_2021-11-15_11-57-31_he1brm.png",
+    repository: "https://github.com/Getsuga743/crypto-barrani",
+    technologies: ["Nextjs", "Chakra-ui", "Styled-components", "Swr", "React"],
+    description:
+      "Aplicación web, hecha con Next.js , Un dashboard donde ver distintos datos sobre la economía argentina y criptomonedas"
+  },
+  {
+    title: "React Journal",
+    url: "https://journal-react-app.vercel.app/",
+    img: "https://res.cloudinary.com/ds3tqgxy5/image/upload/c_fit,h_480,w_768/v1636988587/projects/Captura_de_pantalla_de_2021-11-15_12-01-43_ydywxq.png",
+    repository: "https://github.com/Getsuga743/Journal-React-App",
+    technologies: ["React", "Sass", "Redux", "Node", "Firebase"],
+    description: "Pequeña spa, un diario personal web, firebase como db."
+  },
+  {
+    title: "Easy landing Bank",
+    url: "https://getsuga743.github.io/Easybank-landing/",
+    img: "https://res.cloudinary.com/dz209s6jk/image/upload/c_fit,h_480,w_768/q_auto:good,w_900/Challenges/yezt1f56cfp2njnakpbo.jpg",
+    repository: "https://github.com/Getsuga743/Easybank-landing",
+    technologies: ["JS", "Sass", "HTML5"],
+    description:
+      'Reto de <a href="https://www.frontendmentor.io/challenges">https://www.frontendmentor.io/challenges</a>. Maquetado de una landing page para un banco, hecha con html, sass, y javascript. Diseño full responsive.'
+  },
+  {
+    title: "BuenaBit",
+    url: "https://getsuga743.github.io/BuenaBit/",
+    img: "https://res.cloudinary.com/ds3tqgxy5/image/upload/c_fit,h_480,w_768/v1636988718/projects/Captura_de_pantalla_de_2021-11-15_12-04-00_hkr7dr.png",
+    repository: "https://github.com/Getsuga743/BuenaBit",
+    technologies: ["Html", "Css", "Js"],
+    description:
+      "Maquetado de una landing para un proyecto de criptomonedas, diseño full responsive, y mobile first."
+  }
+];
